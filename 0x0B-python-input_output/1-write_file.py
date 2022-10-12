e15d390529a48c5dcf4981320cb597f18bdc9dfd
Eben-123 <ebenezeretsiwah@gmail.com>
2022-10-12 02:41:01 -0700

    Python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,3 +8,5 @@ def write_file(filename="", text=""):
     """returns the number of chars written to "filename" from "text" """
     with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
+~
+
